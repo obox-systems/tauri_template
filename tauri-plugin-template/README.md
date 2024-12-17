b3e87eb8b6230acd4c383d38f3f4010e35ff2b60
NYBACHOK <nybachokua@gmail.com>
2024-12-17 12:24:37 +0200

    Template for android plugin

--- tauri-plugin-template/README.md
+++ tauri-plugin-template/README.md
@@ -7,3 +7,50 @@ This crate demonstrates example of plugin creation for tauri.
 Read more about how to create plugins in [this](https://tauri.app/develop/plugins) guide.
 
 For quickstart in mobile plugin development you may read [this](https://tauritutorials.com/blog/develop-a-tauri-plugin-for-android) guide.
+
+## Init android support
+
+Execute following in plugin root directory.
+
+```bash
+cargo tauri plugin android init
+```
+
+It should print:
+
+```rust
+// You must add the following to the Cargo.toml file:
+
+[build-dependencies]
+tauri-build = "2.0.2"
+
+// You must add the following code to the build.rs file:
+
+const COMMANDS: &[&str] = &["ping"];
+
+fn main() {
+  tauri_plugin::Builder::new(COMMANDS)
+    .android_path("android")
+    .ios_path("ios")
+    .build();
+}
+
+// Your plugin's init function under src/lib.rs must initialize the Android plugin:
+
+pub fn init<R: Runtime>() -> TauriPlugin<R> {
+  Builder::new("template")
+    .setup(|app, api| {
+      #[cfg(target_os = "android")]
+      let handle = api.register_android_plugin("com.plugin.template", "ExamplePlugin")?;
+      Ok(())
+    })
+    .build()
+}
+```
+
+Makes sure that you have `tauri-build` in deps or `tauri-plugin` has `build` feature enabled.
+Also add identifier of your android project to `init` fn in `mobile` module.
+
+## Remove android support
+
+Remove android directory and remove plugin identifier from `init` fn in `mobile` module.
--- tauri-plugin-template/src/mobile.rs
+++ tauri-plugin-template/src/mobile.rs
@@ -1,7 +1,7 @@
 use serde::de::DeserializeOwned;
 use tauri::{
-  plugin::{PluginApi, PluginHandle},
-  AppHandle, Runtime,
+    plugin::{PluginApi, PluginHandle},
+    AppHandle, Runtime,
 };
 
 use crate::models::*;
@@ -11,24 +11,23 @@ tauri::ios_plugin_binding!(init_plugin_template);
 
 // initializes the Kotlin or Swift plugin classes
 pub fn init<R: Runtime, C: DeserializeOwned>(
-  _app: &AppHandle<R>,
-  api: PluginApi<R, C>,
+    _app: &AppHandle<R>,
+    api: PluginApi<R, C>,
 ) -> crate::Result<Template<R>> {
-  #[cfg(target_os = "android")]
-  let handle = api.register_android_plugin("", "ExamplePlugin")?;
-  #[cfg(target_os = "ios")]
-  let handle = api.register_ios_plugin(init_plugin_template)?;
-  Ok(Template(handle))
+    #[cfg(target_os = "android")]
+    let handle = api.register_android_plugin("com.plugin.template", "ExamplePlugin")?;
+    #[cfg(target_os = "ios")]
+    let handle = api.register_ios_plugin(init_plugin_template)?;
+    Ok(Template(handle))
 }
 
 /// Access to the template APIs.
 pub struct Template<R: Runtime>(PluginHandle<R>);
 
 impl<R: Runtime> Template<R> {
-  pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
-    self
-      .0
-      .run_mobile_plugin("ping", payload)
-      .map_err(Into::into)
-  }
+    pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+        self.0
+            .run_mobile_plugin("ping", payload)
+            .map_err(Into::into)
+    }
 }
